020a9b30c87164959b9167123b939d912da59053
brunanunes32 <nunesbrunacorrea@gmail.com>
2024-10-27 03:43:49 +0400

    Add files via upload

---
+++ donatepage/js/9844-04024d5001395616.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9844],{19844:function(t,n,e){e.d(n,{LV:function(){return a},KP:function(){return o}});var r=e(11527),i=e(47458),s=e.n(i);function a(){return(0,r.jsx)(s(),{id:"hotjar",children:`
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:2940228,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `})}var h=e(40698),c=e(91886),o=function(t){let{locale:n}=t;return"test01"!==h.config.environment.host||n!==c.Qv.ENGLISH_US?null:(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(s(),{type:"text/javascript",src:"//d2c7xlmseob604.cloudfront.net/tracker.min.js"}),(0,r.jsx)(s(),{id:"smartling-context-capturer",strategy:"lazyOnload",children:`
+                if (typeof SmartlingContextTracker !== "undefined" && typeof SmartlingContextTracker.init === "function") {
+                    SmartlingContextTracker.init({ 
+                        orgId: 'TqWSOKKXjisGYiqfs-FaIA',
+                        snapshotSizeLimitBytes: 4000000
+                    });
+                }
+            `})]})}}}]);
+//# sourceMappingURL=9844-04024d5001395616.js.map
